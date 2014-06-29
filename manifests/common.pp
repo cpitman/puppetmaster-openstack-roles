0bbc6a748ebc5ca9b819a2848defdb2fb95f05d1
Chris Pitman <cpitman@redhat.com>
2014-06-29 00:24:59 -0400

    Added common role

---
+++ manifests/common.pp
@@ -0,0 +1,25 @@
+class role::common
+{
+  user { 'cpitman':
+    ensure   => present,
+    password => '$6$3xEmbtIKz4R1ivAp$rOcUwwY5bm6G8YKwYaLrEM61mRZ2hQDFnZJVY.k6dISdOkix/KNLTLI/N4.SpAJ0/IOEZZr4sMtpWh67vC4jJ.',
+    groups   => 'wheel'
+  }
+  
+  user { 'jculleton':
+    ensure   => present,
+    password => '$6$a/uVfwn8Cqzorv/Q$ytXRW88xoHHoQL23CSjWgHf1gKMcodwK5uEFyb607lWu8GYIbO181LvVl/QV1D7NWlkLPCKfrVSWPHANYVRHn0',
+    groups   => 'wheel'
+  }
+  
+  service { 'iptables':
+    ensure => stopped,
+    enable => false
+  }
+  
+  service { 'NetworkManager':
+    ensure => stopped,
+    enable => false
+  }
+  
+}
