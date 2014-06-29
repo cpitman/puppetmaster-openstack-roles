c3e9229e6d5dc916a913fdf855ace8eb45d7fa89
jcullet <john.culleton@gmail.com>
2014-06-29 01:42:10 -0400

    fixed naming on networking class

--- manifests/networking.pp
+++ manifests/networking.pp
@@ -1,4 +1,4 @@
-class networking ($device_name, $mgmt_ip_addr, $instance_ip_addr) {
+class role::networking ($device_name, $mgmt_ip_addr, $instance_ip_addr) {
   $mac_addr = getvar("macaddress_${device_name}")
   file { "/etc/sysconfig/network-scripts/ifcfg-${device_name}":
     content => templace('role/ifcfg.erb')
