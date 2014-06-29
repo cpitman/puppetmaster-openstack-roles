cc6013cf989727ffae5c913a78f84ab3dfaff77a
jcullet <john.culleton@gmail.com>
2014-06-29 01:46:56 -0400

    fixed misspelling in networking module

--- manifests/networking.pp
+++ manifests/networking.pp
@@ -1,6 +1,6 @@
 class role::networking ($device_name, $mgmt_ip_addr, $instance_ip_addr) {
   $mac_addr = getvar("macaddress_${device_name}")
   file { "/etc/sysconfig/network-scripts/ifcfg-${device_name}":
-    content => templace('role/ifcfg.erb')
+    content => template('role/ifcfg.erb')
   }
 }
