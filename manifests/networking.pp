473d1141763dd207507a4693b4957182661181a1
jcullet <john.culleton@gmail.com>
2014-06-29 01:37:07 -0400

    added ifcfg networking template and class

---
+++ manifests/networking.pp
@@ -0,0 +1,6 @@
+class networking ($device_name, $mgmt_ip_addr, $instance_ip_addr) {
+  $mac_addr = getvar("macaddress_${device_name}")
+  file { "/etc/sysconfig/network-scripts/ifcfg-${device_name}":
+    content => templace('role/ifcfg.erb')
+  }
+}
